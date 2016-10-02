389a972bea994d4175cefc9c2055126745a281c4
iplus <ivan.plyusnin@gmail.com>
2016-10-02 19:40:45 +0300

    Add new layers to spacemacs

---
+++ dotfiles/emacs/private/clojure-plus/packages.el
@@ -0,0 +1,10 @@
+(setq clojure-plus-packages '((parinfer-mode :location (recipe :fetcher github
+                                                               :repo "edpaget/parinfer-mode"))))
+
+(defun clojure-plus/init-parinfer-mode ()
+  ;; (use-package parinfer-mode
+  ;;              ;; :defer t
+  ;;              ;; :mode ("\\.php\\'" . php-mode)
+  ;;              :init (progn
+  ;;                      (add-hook 'clojure-mode-hook #'parinfer-mode)) )
+  )
