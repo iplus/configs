8cade52f99a1bb89553ee068a0649a78d0e8cbfe
Ivan Plyusnin <ivan.plyusnin@gmail.com>
2017-01-09 16:59:10 +0300

    Cleanup php layer

--- dotfiles/emacs/private/semanticphp/packages.el
+++ dotfiles/emacs/private/semanticphp/packages.el
@@ -1,45 +1,15 @@
 (setq semanticphp-packages '(cedet
-                             ede-php-autoload
-                              ;; (ede-php-autoload :location (recipe :fetcher github
-                              ;;                                     :repo "stevenremot/ede-php-autoload"))
-                              (semantic-php :location (recipe :fetcher github
-                                                              :repo "trashofmasters/semantic-php"))))
-(defun semanticphp/init-cedet ()
-  (use-package cedet))
-
-(defun semanticphp/init-ede-php-autoload ()
-   (add-hook 'php-mode-hook #'ede-php-autoload-mode))
-
-;;(defun semanticphp/init-ede-php-autoload ()
-  ;; (add-hook 'php-mode-hook #'ede-php-autoload-mode)
-;;  (use-package ede-php-autoload
-    ;; :defer t
-  ;;   ;; :mode ("\\.php\\'" . php-mode)
-  ;;   ;; :init (push "~/.emacs.d/" load-path)
-;;    :load-path "~/.emacs.d/private/semantic-php/ede-php-autoload/"
-;;    :config (progn
-;;              (global-ede-mode t)
-  ;;             (require 'ede-php-autoload)
-  ;;             (require 'ede-php-autoload-mode)
-;;              (add-hook 'php-mode-hook #'ede-php-autoload-mode)
-;;              ))
-  ;; (use-package ede-php-autoload
-    ;; :commands ede-php-autoload-mode
-    ;; :load-path "~/.emacs.d/ede-php-autoload/"
-    ;; :init (progn
-    ;;         (add-hook 'php-mode-hook 'global-ede-mode)
-    ;;         (add-hook 'php-mode-hook 'ede-php-autoload-mode)
-    ;;         )
-    ;; :config (ede-php-autoload-project "My project"
-    ;;                                   :file "~/dev/yorso/yorso")
-    ;; )
-  ;; )
-;;  )
+                             ;; ede-php-autoload
+                             ;; (ede-php-autoload :location (recipe :fetcher github
+                             ;;                                     :repo "stevenremot/ede-php-autoload"))
+                             (semantic-php :location (recipe :fetcher github
+                                                             :repo "trashofmasters/semantic-php"))))
+(defun semanticphp/init-cedet ())
 
 (defun semanticphp/init-semantic-php ()
   (use-package semantic-php
-               ;; :defer t
-               ;; :mode ("\\.php\\'" . php-mode)
-               :init (progn
-                         (require 'grammar-setup)
-                         (add-hook 'php-mode-hook #'semantic-mode)) ))
+    :defer t
+    :mode ("\\.php\\'" . php-mode)
+    :init (progn
+            (require 'grammar-setup)
+            (add-hook 'php-mode-hook #'semantic-mode)) ))
