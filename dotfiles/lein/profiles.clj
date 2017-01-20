81ee7829bed5ceec8e785fb41d0d5bd6a9c03ce8
Ivan Plyusnin <ivan.plyusnin@gmail.com>
2017-01-20 11:34:40 +0300

    Add TAGS generation to clojure projects

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -2,8 +2,14 @@
                   [lein-kibit "0.1.3"]
                   [com.billpiel/sayid "0.0.10"]
                   [lein-bikeshed "0.4.1"]
+                  [lein-codeindex "0.1.0"]
                   [lein-checkall "0.1.1"]
                   [lein-repetition-hunter "0.1.0-SNAPSHOT"]]
         :dependencies [[spyscope "0.1.5"]]
+        ;; [funcool/cuerdas "2.0.2"] ;; string manipulation
+        ;; [manifold "0.1.5"] ;; asynchronous programming
+        ;; [tulos/manifail "0.4.0"]
+        ;; [tracks "1.0.3"] ;; transformations by example
+        ;; [spectrum "0.1.1"] ;; spec static analysis
         :injections [(require 'spyscope.core)]}
  :repl {:dependencies [[acyclic/squiggly-clojure "0.1.7"]]}}
